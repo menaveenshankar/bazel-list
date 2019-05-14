be655bdbea441922eb383ecf817bde9f079123f9
Naveen Shankar Nagaraja <nagaraja@cs.uni-freiburg.de>
2019-05-14 16:57:02 +0200

    simple regex to parse BUILD
    this regex checks for <rule_type>(<any char till new line>
    which captures only till the name of the rule
    e.g. cc_binary(
               name = "cool_pkg",

--- bzlst.py
+++ bzlst.py
@@ -4,10 +4,44 @@ import sys
 import re
 import os, glob
 import argparse
+import itertools
+
+regex_rule = '\(\\n.*'
 
 
 def find_build_files(path):
     return glob.iglob(path + '/**/BUILD', recursive=True)
+
+
+def extract_rule_name(rule_list):
+    # TODO: check if try catch works within list comprehension in py3
+    result = []
+    for x in rule_list:
+        try:
+            result.append(x.split('"')[1])
+        except:
+            pass
+    return result
+
+
+def extract_specific_rule(rule_type, content, option_idx, target_path):
+    colour_attrib = lambda x, i: colours[i % len(colours)] + x + default_colour
+
+    # TODO: find a better way to lex+parse, currently super hacky!
+    rules = re.findall('{}{}'.format(rule_type, regex_rule), content)
+    rule_names = extract_rule_name(rules)
+
+    rule_fmt_disp = [output_format(x, colour_attrib(rule_type[:6], option_idx), target_path) for x in
+                     rule_names] if rule_names else []
+    return rule_fmt_disp
+
+
+def extract_bazel_rules(filename, ws_dir, options):
+    content = open(filename, 'r').read()
+    target_path = '/' + filename.split(ws_dir)[1]
+
+    output_display = [extract_specific_rule(opt, content, i, target_path) for i, opt in enumerate(options)]
+    return itertools.chain(*output_display)
 
 
 if __name__ == '__main__':
